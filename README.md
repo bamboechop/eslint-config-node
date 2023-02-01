51318dd6e53ee77f35fe1a511da90c420693d2eb
bamboechop <info@bamboechop.at>
2023-02-01 12:22:14 +0100

    1.0.0

---
+++ README.md
@@ -0,0 +1,19 @@
+# @bamboechop/eslint-config-nestjs
+
+This ESLint config package contains my preferred set of ESLint rules for a Nest.js based backend project.
+
+## Installation
+
+```
+npm install --save-dev @bamboechop/eslint-config-nestjs
+```
+
+## Usage
+
+Add the `extends` array to your ESLint config file (e.g. `.eslintrc.js) and reference this package.
+
+```
+extends: [
+  `@ringana/eslint-config-nest`,
+]
+```
---
+++ index.js
@@ -0,0 +1,138 @@
+module.exports = {
+  env: {
+    node: true,
+    jest: true,
+  },
+  extends: [
+    `plugin:@typescript-eslint/eslint-recommended`,
+    `plugin:@typescript-eslint/recommended`,
+    `airbnb-base`,
+  ],
+  ignorePatterns: [`.eslintrc.js`, `/migrations/*`],
+  parser: `@typescript-eslint/parser`,
+  parserOptions: {
+    project: `tsconfig.json`,
+    sourceType: `module`,
+  },
+  plugins: [
+    `@typescript-eslint/eslint-plugin`,
+    `import-quotes`,
+  ],
+  root: true,
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': `error`,
+    '@typescript-eslint/explicit-module-boundary-types': `off`,
+    '@typescript-eslint/member-delimiter-style': [`error`, {
+      multiline: {
+        delimiter: `semi`,
+        requireLast: true,
+      },
+      singleline: {
+        delimiter: `semi`,
+        requireLast: false,
+      },
+    }],
+    '@typescript-eslint/member-ordering': [
+      `warn`,
+      {
+        default: {
+          memberTypes: [
+            `public-field`,
+            `protected-field`,
+            `private-field`,
+            `constructor`,
+            `get`,
+            `set`,
+            `public-method`,
+            `protected-method`,
+            `private-method`,
+          ],
+        },
+        interfaces: {
+          order: `alphabetically`,
+        },
+        typeLiterals: {
+          order: `alphabetically`,
+        },
+      },
+    ],
+    '@typescript-eslint/naming-convention': [
+      `error`,
+      {
+        custom: {
+          match: true,
+          regex: `^E[A-Z]`,
+        },
+        format: [`PascalCase`],
+        selector: `enum`,
+      },
+      {
+        custom: {
+          match: true,
+          regex: `^I[A-Z]`,
+        },
+        format: [`PascalCase`],
+        selector: `interface`,
+      },
+      {
+        custom: {
+          regex: '^T[A-Z]',
+          match: true
+        },
+        format: ['PascalCase'],
+        selector: 'typeAlias',
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': `error`,
+    '@typescript-eslint/no-unused-vars': [`warn`, { argsIgnorePattern: `^_` }],
+    '@typescript-eslint/no-useless-constructor': `error`,
+    '@typescript-eslint/return-await': `error`,
+    '@typescript-eslint/semi': [`warn`],
+    'arrow-body-style': `off`,
+    'arrow-parens': `off`,
+    'class-methods-use-this': `off`,
+    'import/extensions': `off`,
+    'import/no-extraneous-dependencies': `off`,
+    'import/no-unresolved': `off`,
+    'import/prefer-default-export': `off`,
+    'import-quotes/import-quotes': [1, `single`],
+    'keyword-spacing': [`error`, {
+      after: true,
+      before: true,
+      overrides: {
+        catch: { after: false },
+        for: { after: false },
+        if: { after: false },
+        switch: { after: false },
+      },
+    }],
+    'linebreak-style': `off`,
+    'lines-between-class-members': `off`,
+    'max-classes-per-file': `off`,
+    'max-len': [`warn`, { code: 200 }],
+    'no-console': `warn`,
+    'no-empty-function': [`error`, { allow: [`constructors`] }],
+    'no-plusplus': [`error`, { allowForLoopAfterthoughts: true }],
+    'no-restricted-syntax': [
+      `warn`,
+      `ForInStatement`,
+      `LabeledStatement`,
+      `WithStatement`,
+    ],
+    'no-return-await': `off`,
+    'no-shadow': `off`,
+    'no-unused-vars': [`warn`, { argsIgnorePattern: `^_` }],
+    'no-useless-constructor': `off`,
+    'no-void': `off`,
+    quotes: [`error`, `backtick`],
+    semi: `off`,
+    'sort-keys': [`warn`, `asc`],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: [`.js`, `.jsx`, `.ts`, `.tsx`],
+      },
+    },
+  },
+};
